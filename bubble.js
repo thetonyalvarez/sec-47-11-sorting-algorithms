9ec5545de8b052f17682a2250091d0d2b0d88642
Tony Alvarez <tonyalva87@gmail.com>
2022-05-10 20:37:44 -0700

    completed bubble sort exercies

--- bubble.js
+++ bubble.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		console.log("i =", arr[i]);
+		for (let j = 0; j < i; j++) {
+			console.log("j =", arr[j]);
+			if (arr[j] > arr[i]) {
+				let k = arr[j];
+				arr[j] = arr[i];
+				arr[i] = k;
+			}
+		}
+	}
+	return arr;
+}
 
 module.exports = bubbleSort;
