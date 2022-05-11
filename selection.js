faef632d3680011ea99a9767a720be91fbde52ff
Tony Alvarez <tonyalva87@gmail.com>
2022-05-10 20:38:54 -0700

    completed selection sort test

--- selection.js
+++ selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort(arr) {
+	for (let start = 0; start < arr.length - 1; start++) {
+		console.log("start =", arr[start]);
+
+		for (let end = arr.length - 1; end > start; end--) {
+			console.log("end =", arr[end]);
+
+			if (arr[start] > arr[end]) {
+				let k = arr[end];
+				arr[end] = arr[start];
+				arr[start] = k;
+			}
+		}
+	}
+	return arr;
+}
 
 module.exports = selectionSort;
